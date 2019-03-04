4e3c0624987d642f101676c7ed4354f6a1844e49
Yassin AJDI <yassin.ajdi@gmail.com>
2019-03-04 21:06:46 +0100

    Add java joke library

---
+++ JavaJokerLib/src/main/java/com/ajdi/yassin/javajokerlib/Joker.java
@@ -0,0 +1,11 @@
+package com.ajdi.yassin.javajokerlib;
+
+public class Joker {
+
+    public String tellAJoke() {
+        return "Q. How does a computer get drunk?\n" +
+                "\n" +
+                "A. It takes screen shots!\n" +
+                "\n";
+    }
+}
