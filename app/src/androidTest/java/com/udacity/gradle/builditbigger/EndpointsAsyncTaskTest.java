1b2b49fea593e4673ad4ab1e039ee510c1cfba1d
Yassin AJDI <yassin.ajdi@gmail.com>
2019-03-08 15:45:46 +0100

    Add test to verify asynkTask loading actual jokes

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsAsyncTaskTest.java
@@ -1,10 +1,15 @@
 package com.udacity.gradle.builditbigger;
 
 import android.support.test.runner.AndroidJUnit4;
+import android.text.TextUtils;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import java.util.concurrent.CountDownLatch;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * @author Yassin Ajdi
@@ -13,5 +18,26 @@ import static org.junit.Assert.*;
 @RunWith(AndroidJUnit4.class)
 public class EndpointsAsyncTaskTest {
 
+    private String result = null;
 
+    @Test
+    public void jokeLoadedTest() {
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        new EndpointsAsyncTask(new JokeListener() {
+            @Override
+            public void onJokeLoaded(String joke) {
+                result = joke;
+                countDownLatch.countDown();
+            }
+        }).execute();
+
+        try {
+            countDownLatch.await();
+            assertNotNull("joke is null", result);
+            assertFalse("joke is empty", TextUtils.isEmpty(result));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
