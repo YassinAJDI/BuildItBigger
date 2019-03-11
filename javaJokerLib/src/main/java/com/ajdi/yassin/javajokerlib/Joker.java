c9aa5811cfd058791a899ccded8c0de70588cc47
Yassin AJDI <yassin.ajdi@gmail.com>
2019-03-11 15:51:41 +0100

    Fix visual bugs

--- javaJokerLib/src/main/java/com/ajdi/yassin/javajokerlib/Joker.java
+++ javaJokerLib/src/main/java/com/ajdi/yassin/javajokerlib/Joker.java
@@ -3,9 +3,9 @@ package com.ajdi.yassin.javajokerlib;
 public class Joker {
 
     public String tellAJoke() {
-        return "Q. What’s the difference between a guitar and a fish?\n" +
+        return "Q. Whats the difference between a guitar and a fish?\n" +
                 "\n" +
-                "A. You can’t tuna fish.\n" +
+                "A. You cant tuna fish.\n" +
                 "\n";
     }
 }
